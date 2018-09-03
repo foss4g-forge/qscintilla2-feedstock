a9054c193715b20993d54003404d135431dd003e
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-09-03 17:04:58 -0600

    Update to v 2.10; add Qt 5.9 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,7 @@ fi
 
 # Set build specs depending on current platform (Mac OS X or Linux)
 if [ `uname` == Darwin ]; then
-	BUILD_SPEC=macx-llvm
+	BUILD_SPEC=macx-clang
 else
 	BUILD_SPEC=linux-g++
 fi
@@ -21,7 +21,7 @@ fi
 cd ${SRC_DIR}/Qt4Qt5
 # Build the makefile with qmake, specify llvm as the compiler
 # The normal g++ compiler on Mac causes an __Unwind_Resume error at linking phase
-${BIN}/qmake qscintilla.pro -spec ${BUILD_SPEC}
+${BIN}/qmake qscintilla.pro -config release -spec ${BUILD_SPEC}
 # Build Qscintilla
 make
 # and install it
@@ -32,13 +32,13 @@ make install
 # Go to python folder
 cd ${SRC_DIR}/Python
 # Configure compilation of Python Qsci module
-${PYTHON} configure.py --pyqt=PyQt${QT_MAJOR_VER} --qmake=${BIN}/qmake --sip=${BIN}/sip --qsci-incdir=${PREFIX}/include/qt --qsci-libdir=${PREFIX}/lib --spec=${BUILD_SPEC} --no-qsci-api 
+${PYTHON} configure.py --pyqt=PyQt${QT_MAJOR_VER} --qmake=${BIN}/qmake --sip=${BIN}/sip --qsci-incdir=${PREFIX}/include/qt --qsci-libdir=${PREFIX}/lib --spec=${BUILD_SPEC} --no-qsci-api
 # make it
 make
 
 # On OSX: Change reference from libQsci.dylib to Qsci.so (otherwise anaconda linker crashes)
 if [ `uname` == Darwin ]; then
-	install_name_tool -id Qsci.so Qsci.so
+	install_name_tool -id Qsci.so Qsci/Qsci.so
 fi
 
 # Install QSci.so to the site-packages/PyQt4 folder
