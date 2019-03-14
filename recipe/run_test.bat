6426e214a30a2707b195a1f6de41b1dfd9293435
Chris Holden <ceholden@gmail.com>
2019-03-14 13:35:09 -0400

    Fix test on Windows

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -3,7 +3,7 @@
 setlocal EnableDelayedExpansion
 
 :: Determine Qt Major verion (4 or 5)
-for /f "tokens=4" %%i in ('%LIBRARY_BIN%\qmake -v ^| find "Qt version"') do (
+for /f "tokens=4" %%i in ('qmake -v ^| find "Qt version"') do (
 	set QT_MAJOR_VER=%%i
 	set QT_MAJOR_VER=!QT_MAJOR_VER:~0,1!
 )
